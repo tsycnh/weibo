e5bdf8a28ce45724063fd92191face6b72c958b6
tsycnh <tsycnh@126.com>
2022-09-01 17:01:06 +0800

    创建注册页面

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/','StaticPagesController@home')->name('home');//第一个参数明确url，第二个参数明确URL的控制器动作
 Route::get('/help','StaticPagesController@help')->name('help');
 Route::get('/about','StaticPagesController@about')->name('about');
+
+Route::get('signup','UsersController@create')->name('signup');
