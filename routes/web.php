61bf81e5948c80b42a9afb8cca32e021be76bcd7
tsycnh <tsycnh@126.com>
2022-09-01 16:55:42 +0800

    命名路由

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/','StaticPagesController@home');//第一个参数明确url，第二个参数明确URL的控制器动作
-Route::get('/help','StaticPagesController@help');
-Route::get('/about','StaticPagesController@about');
+Route::get('/','StaticPagesController@home')->name('home');//第一个参数明确url，第二个参数明确URL的控制器动作
+Route::get('/help','StaticPagesController@help')->name('help');
+Route::get('/about','StaticPagesController@about')->name('about');
